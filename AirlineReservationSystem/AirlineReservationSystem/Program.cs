35bd670682302a705446d0c0a9e5f1a857fdcf0e
ghulamghousdev <itsghulamghous@gmail.com>
2020-09-02 02:51:35 +0500

    Updated

--- AirlineReservationSystem/AirlineReservationSystem/Program.cs
+++ AirlineReservationSystem/AirlineReservationSystem/Program.cs
@@ -16,7 +16,7 @@ namespace AirlineReservationSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Dashboard());
+            Application.Run(new Form5());
         }
     }
 }
